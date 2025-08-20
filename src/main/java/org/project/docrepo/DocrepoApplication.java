2e91a96053c663f7a7aebe3a96799b3f0cc66a96
puitandysatyam <satyam287b@gmail.com>
2025-08-20 21:16:28 +0530

    initial commit

---
+++ src/main/java/org/project/docrepo/DocrepoApplication.java
@@ -0,0 +1,13 @@
+package org.project.docrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocrepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocrepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/docrepo/DocrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.docrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocrepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
