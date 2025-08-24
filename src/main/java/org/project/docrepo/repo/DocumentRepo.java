69eafef4f59ee6707337dd53c9761b48fc517eaf
puitandysatyam <satyam287b@gmail.com>
2025-08-24 17:54:37 +0530

    Document Repo : add specific query

--- src/main/java/org/project/docrepo/repo/DocumentRepo.java
+++ src/main/java/org/project/docrepo/repo/DocumentRepo.java
@@ -2,6 +2,7 @@ package org.project.docrepo.repo;
 
 import org.project.docrepo.model.Documents;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,9 @@ public interface DocumentRepo extends MongoRepository<Documents, String> {
     List<Documents> findByTitle(String title);
     List<Documents> findByFacultyId(String facultyId);
     List<Documents> findByTopic(String topic);
+
+    @Query("{'$or':[ {'title': {'$regex': ?0, '$options': 'i'}}, {'topic': {'$regex': ?0, '$options': 'i'}}, {'facultyName': {'$regex': ?0, '$options': 'i'}} ]}")
+    List<Documents> searchByTopicTitleOrFaculty(String query);
 
 
 }
