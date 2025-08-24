9181de90119816f08df65a696741564417a40d74
puitandysatyam <satyam287b@gmail.com>
2025-08-24 18:40:06 +0530

    Controller : rename DepartmentController to BrowseCOntroller

--- src/main/java/org/project/docrepo/controllers/BrowseController.java
+++ src/main/java/org/project/docrepo/controllers/BrowseController.java
@@ -1,19 +1,24 @@
 package org.project.docrepo.controllers;
 
 import org.project.docrepo.model.Department;
+import org.project.docrepo.model.User;
 import org.project.docrepo.services.DepartmentService;
+import org.project.docrepo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @Controller
-public class DepartmentsController {
+public class BrowseController {
 
     @Autowired
     DepartmentService departmentService;
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/departments")
     public String showDepartments(Model model){
@@ -22,6 +27,17 @@ public class DepartmentsController {
         model.addAttribute("departments", departments);
 
         return "departments";
+
+    }
+
+    @GetMapping("/departments/{deptName}/faculty")
+    public String showFaculty(Model model, @PathVariable String deptName){
+
+        List<User> faculty = userService.findFacultyByDept(deptName);
+        model.addAttribute("facultyMembers", faculty);
+        model.addAttribute("departmentName",deptName);
+
+        return "faculty-list";
 
     }
 }
