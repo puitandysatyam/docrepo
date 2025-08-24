4c0ac11e5505a80ec4255f0635213d2cf32ba4ba
puitandysatyam <satyam287b@gmail.com>
2025-08-24 09:06:43 +0530

    repository : make changes to UserRepository

--- src/main/java/org/project/docrepo/repo/UserRepo.java
+++ src/main/java/org/project/docrepo/repo/UserRepo.java
@@ -3,12 +3,17 @@ package org.project.docrepo.repo;
 
 import org.project.docrepo.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface UserRepo extends MongoRepository<User, String> {
 
     String findEmailById(String Id);
     List<User> findByDepartment(String department);
+    Optional<User> findByEmail(String email);
 
+    boolean existsByEmail(String email);
 }
