d6b4a2e0f79330a7d7dba162ef5a9cfb69c7e854
puitandysatyam <satyam287b@gmail.com>
2025-08-24 18:00:02 +0530

    Model : add Department Model

--- src/main/java/org/project/docrepo/model/Department.java
+++ src/main/java/org/project/docrepo/model/Department.java
@@ -1,7 +1,9 @@
 package org.project.docrepo.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "Departments")
 public class Department {
 
     @Id
