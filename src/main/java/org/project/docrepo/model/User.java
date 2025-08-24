7f00e0d1de5f3ca33e271f3188c1336bd95b772e
puitandysatyam <satyam287b@gmail.com>
2025-08-24 09:07:00 +0530

    model : make changes to User Model

--- src/main/java/org/project/docrepo/model/User.java
+++ src/main/java/org/project/docrepo/model/User.java
@@ -2,24 +2,31 @@ package org.project.docrepo.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 
 @Document(collection = "User")
-public class User {
+public class User implements UserDetails {
 
     @Id
     String id;
-    String name,  email, password, role, department;
+    String fullName,  email, password, role, department, profileImageUrl;
     public User() {
     }
 
-    public User(String id, String name, String email, String password, String role, String department) {
+    public User(String id, String fullName, String email, String password, String role, String department, String profileImageUrl) {
         this.id = id;
-        this.name = name;
+        this.fullName = fullName;
         this.email = email;
         this.password = password;
         this.role = role;
         this.department = department;
+        this.profileImageUrl = profileImageUrl;
     }
 
     public String getId() {
@@ -30,12 +37,12 @@ public class User {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getFullName() {
+        return fullName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 
     public String getEmail() {
@@ -68,5 +75,39 @@ public class User {
 
     public void setDepartment(String department) {
         this.department = department;
+    }
+
+    public String getProfileImageUrl(){ return profileImageUrl; }
+
+    public void setProfileImageUrl( String profileImageUrl){ this.profileImageUrl = profileImageUrl; }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_"+this.role));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.fullName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
