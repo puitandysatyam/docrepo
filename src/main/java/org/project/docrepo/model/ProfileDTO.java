2d66577ae873701311e63924e80b78e8a6f60853
puitandysatyam <satyam287b@gmail.com>
2025-09-02 00:23:52 +0530

    Data Transfer Object: added ProfileDTO

---
+++ src/main/java/org/project/docrepo/model/ProfileDTO.java
@@ -0,0 +1,63 @@
+package org.project.docrepo.model;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class ProfileDTO {
+
+    @NotBlank
+    String fullName;
+
+    @NotBlank
+    String email;
+
+    String profileDescription;
+
+    @NotBlank
+    String department;
+
+    @NotBlank
+    String profileImageUrl;
+
+    public ProfileDTO() {
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public String getProfileDescription() {
+        return profileDescription;
+    }
+
+    public void setProfileDescription(String profileDescription) {
+        this.profileDescription = profileDescription;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
