119005360541d9dd899eb76153e27b955b185c0f
puitandysatyam <satyam287b@gmail.com>
2025-09-02 00:25:26 +0530

    Profile Controller : added edit profile endpoint and changed the view profile endpoint

--- src/main/java/org/project/docrepo/controllers/ProfileController.java
+++ src/main/java/org/project/docrepo/controllers/ProfileController.java
@@ -1,15 +1,19 @@
 package org.project.docrepo.controllers;
 
 
+import jakarta.validation.Valid;
 import org.project.docrepo.model.Documents;
+import org.project.docrepo.model.ProfileDTO;
 import org.project.docrepo.model.User;
 import org.project.docrepo.services.DocumentService;
 import org.project.docrepo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -21,15 +25,50 @@ public class ProfileController {
     @Autowired
     private DocumentService documentService;
 
-    @GetMapping("/profile/{id}")
-    public String viewProfile(Model model, @PathVariable String id){
+    @GetMapping("/profile")
+    public String viewProfile(Model model, @AuthenticationPrincipal User faculty){
 
-        User faculty = userService.findUserById(id).orElse(null);
         model.addAttribute("faculty", faculty);
-        List<Documents> documents = documentService.findDocById(id);
+        List<Documents> documents = documentService.findDocByFacultyId(faculty.getId());
         model.addAttribute("documents",documents);
 
         return "profile";
+
+    }
+
+    @GetMapping("/profile/edit")
+    public String showEditProfileForm(Model model, @AuthenticationPrincipal User faculty){
+        ProfileDTO profileDTO = new ProfileDTO();
+        profileDTO.setFullName(faculty.getFullName());
+        profileDTO.setDepartment(faculty.getDepartment());
+        profileDTO.setProfileDescription(faculty.getProfileDescription());
+        profileDTO.setEmail(faculty.getEmail());
+        profileDTO.setProfileImageUrl(faculty.getProfileImageUrl());
+
+        model.addAttribute("profileDto", profileDTO);
+
+        return "edit-profile";
+    }
+
+    @PostMapping("/profile/update")
+    public String editProfile(@Valid @ModelAttribute("profileDto") ProfileDTO profileDTO,
+                              Model model,
+                              @AuthenticationPrincipal User existingProfile,
+                              BindingResult bindingResult,
+                              RedirectAttributes redirectAttributes){
+
+        if(bindingResult.hasErrors()){
+            return "edit-profile";
+        }
+        try {
+            userService.updateUser(existingProfile, profileDTO);
+            redirectAttributes.addFlashAttribute("successMessage", "Profile Update Successfully. ");
+            return "redirect:/profile";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage","Failed to update profile. Try again later !");
+            return "redirect:/profile";
+        }
+
 
     }
 }
