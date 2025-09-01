52c1df6f0cd10c26ea4b4814541981a648f10a4a
puitandysatyam <satyam287b@gmail.com>
2025-09-01 23:18:05 +0530

    BrowseController : add show faculty details endpoint

--- src/main/java/org/project/docrepo/controllers/BrowseController.java
+++ src/main/java/org/project/docrepo/controllers/BrowseController.java
@@ -1,8 +1,10 @@
 package org.project.docrepo.controllers;
 
 import org.project.docrepo.model.Department;
+import org.project.docrepo.model.Documents;
 import org.project.docrepo.model.User;
 import org.project.docrepo.services.DepartmentService;
+import org.project.docrepo.services.DocumentService;
 import org.project.docrepo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,6 +21,8 @@ public class BrowseController {
     DepartmentService departmentService;
     @Autowired
     private UserService userService;
+    @Autowired
+    private DocumentService documentService;
 
     @GetMapping("/departments")
     public String showDepartments(Model model){
@@ -39,5 +43,15 @@ public class BrowseController {
 
         return "faculty-list";
 
+    }
+
+    @GetMapping("/browse/{deptName}/{facultyId}")
+    public String showFacultyDetails(Model model , @PathVariable String facultyId){
+
+        User faculty = userService.findUserById(facultyId).orElse(null);
+        model.addAttribute("faculty", faculty);
+        List<Documents> documents = documentService.findDocByFacultyId(facultyId);
+        model.addAttribute("documents", documents);
+        return "faculty-details";
     }
 }
