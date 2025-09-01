47735bf21539525eb52ef1e36d35f5685d9332ba
puitandysatyam <satyam287b@gmail.com>
2025-09-01 23:27:35 +0530

    Repository : added downloadRequest repository

---
+++ src/main/java/org/project/docrepo/repo/DownloadRequestRepo.java
@@ -0,0 +1,17 @@
+package org.project.docrepo.repo;
+
+
+import org.project.docrepo.model.DownloadRequest;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface DownloadRequestRepo extends MongoRepository<DownloadRequest,String> {
+    boolean existsByDocumentIdAndStudentId(String documentId, String id);
+
+    List<DownloadRequest> findAllByStudentId(String id);
+
+    List<DownloadRequest> findAllByFacultyIdContainingAndStatus(String facultyId, String status);
+
+    List<DownloadRequest> findAllByFacultyIdContainingAndStatusNotContaining(String facultyId, String status);
+}
